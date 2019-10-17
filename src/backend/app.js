f27064107c186ca604896b4a6bcfaf1fa3de57db
Alec Schule <aschule@usc.edu>
2019-10-17 13:51:19 -0700

    finish artist name search endpoint

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Endpoints
+var artist_lookup = require('./artist-lookup.js')
+
 // return list of artists matching a partial artist name 
 // /artist-lookup?name=
-app.get('/artist-lookup', require('./artist-lookup.js'));
+app.use('/artist-lookup', artist_lookup);
 
 // return JSON string of the form {“word1”: num1, “word2”: num2, … }
 // app.get('/artist-lyrics', require('./artist-lyrics.js'));
--- src/backend/artist-lookup.js
+++ src/backend/artist-lookup.js
@@ -1,5 +1,34 @@
-// Given a partial artist name string, return an array of artists that match
-module.exports = function(req, res) {
-	res.send(req.query.name);
+const express = require('express')
+var router = express.Router()
+
+const request = require('request')
+const config = require('./config.js')
+
+const last_fm_endpoint = 'http://ws.audioscrobbler.com/2.0/'
+
+router.get('/', function(req, res) {
+    // Search artists on Last FM
+    // Build the URL
+    var url = last_fm_endpoint +
+        '?method=artist.search&artist=' + req.query.name +
+        '&api_key=' + config.last_fm_key +
+        '&limit=5' +
+        '&format=json'
+    request(url, {json: true}, (error, response, body) => {
+        if (error) {
+        }
+        var artist_names = parse_artists(body)
+        res.send(artist_names)
+    })
+})
+
+function parse_artists(body) {
+    var artists = body["results"]["artistmatches"]["artist"]
+    var names = []
+    for (const artist of artists) {
+        names.push(artist["name"])
+    }
+    return names
 }
 
+module.exports = router
